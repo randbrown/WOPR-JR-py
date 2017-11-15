be86c1c261eaa48b9c8f5ee0efb81f054baef58f
Randy Brown <brown.randy@gmail.com>
2017-11-14 19:03:00 -0500

    Simple read analog input 0 for ultrasonic and dump to dash

--- src/subsystems/__init__.py
+++ src/subsystems/__init__.py
@@ -5,6 +5,7 @@ for it in the global scope.
 '''
 
 from wpilib.robotbase import RobotBase
+from wpilib import AnalogInput
 from subsystems.drive import Drive
 import robotmap
 from networktables import NetworkTables
@@ -13,11 +14,13 @@ from robotpy_ext.common_drivers.navx import AHRS
 drive = None
 sd = None
 ahrs = None
+rangefinder = None
 
 def init():
     global drive
     global sd
     global ahrs
+    global rangefinder
     
     ahrs = AHRS.create_spi()
 
@@ -25,10 +28,11 @@ def init():
         raise RuntimeError('Subsystems have already been initialized')
     
     sd = NetworkTables.getTable('SmartDashboard')
-
+    rangefinder = AnalogInput(0)
     drive = Drive(robotmap.motors.L0, robotmap.motors.L1, robotmap.motors.R0, robotmap.motors.R1)
 
 def dumpInfo():
     global ahrs
     sd.putNumber('Navx_getYaw', ahrs.getYaw())
     sd.putNumber('Navx_getPitch', ahrs.getPitch())
+    sd.putNumber('Ultrasonic_getAverageVoltage', rangefinder.getAverageVoltage())
